97a07645abd51190ac078966c6958810ca189c21
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-11-08 12:59:07 -0700

    update heroku

--- knowyourvote/settings.py
+++ knowyourvote/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
 import environ
 env = environ.Env()
